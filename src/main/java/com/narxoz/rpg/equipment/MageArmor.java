c714c3dad2415de877de5763aea4465dd48a1a1b
adiirgimbayevv <adigordienko@gmail.com>
2026-02-13 01:46:35 +0500

    Mage stuff

---
+++ src/main/java/com/narxoz/rpg/equipment/MageArmor.java
@@ -0,0 +1,6 @@
+package com.narxoz.rpg.equipment;
+
+public class MageArmor implements Armor {
+    @Override public int getDefense(){ return 10;}
+    @Override public String getArmorInfo(){ return "Magic Armor"; }
+}
---
+++ src/main/java/com/narxoz/rpg/equipment/MageStf.java
@@ -0,0 +1,6 @@
+package com.narxoz.rpg.equipment;
+
+public class MageStf implements Weapon {
+    @Override public String getWeaponType(){ return "Staff"; }
+    @Override public String getWeaponInfo(){ return "Invincible magic stick"; }
+}
