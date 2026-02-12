efa64829bdd06b4054d8e80b545fb16d8bd96a16
adiirgimbayevv <adigordienko@gmail.com>
2026-02-13 01:07:40 +0500

    class three was added

---
+++ src/main/java/com/narxoz/rpg/character/Healer.java
@@ -0,0 +1,29 @@
+package com.narxoz.rpg.character;
+import com.narxoz.rpg.equipment.Weapon;
+
+public class Healer implements Character{
+    private String name;
+    private int health=90;
+    private int spirit=130; // Особый стат для лекаря
+    private Weapon equippedWeapon;
+    public Healer(String name){
+        this.name=name;
+    }
+    @Override public String getName() { return name; }
+    @Override public int getHealth() { return health;
+    }
+    @Override
+    public void displayStats(){
+        System.out.println("Hero: "+name+" [Healer)");
+        System.out.println("HP: "+health+" | Spirit: "+spirit);
+        if (equippedWeapon!=null){
+            System.out.println("Weapon: "+equippedWeapon.getWeaponInfo());}}
+    @Override
+    public void useSpecialAbility(){
+        System.out.println(name+" uses Heal");
+    }
+    @Override
+    public void equipWeapon(Weapon weapon) {
+        this.equippedWeapon=weapon;
+        System.out.println(name+" equipped "+weapon.getWeaponType());}
+}
